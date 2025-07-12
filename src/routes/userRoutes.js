ae251957a8b3b90a3e5bec66bcbaa1cd093abfc7
Prashant Sachan <your-email@example.com>
2025-07-12 22:02:27 +0530

    add admin authrorization for deactivetUser Routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createUser, deactivateUser, getAllUsers, getUser } from "../controllers/userController.js";
-import { protect } from "../middleware/auth.js";
+import {
+  createUser,
+  deactivateUser,
+  getAllUsers,
+  getUser,
+} from "../controllers/userController.js";
+import { authorize, protect } from "../middleware/auth.js";
 
 const router = express.Router();
 router.post("/users", protect, createUser);
 router.get("/users", protect, getAllUsers);
-router.delete("/users/:id",protect,deactivateUser)
+router.delete("/users/:id", protect, authorize("admin"), deactivateUser);
 
 export default router;
